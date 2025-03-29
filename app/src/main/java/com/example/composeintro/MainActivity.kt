5764b0858c46f44411c93d3fd7e0d7f4b067c718
arjun98k <arjunkandekar14@gmail.com>
2025-03-29 18:18:14 +0530

    image implementation

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -31,8 +33,12 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, widthDp = 300 , heightDp = 500 )
 @Composable
 private fun PreviewFunction() {
-    Text(text = "hello Arjun", fontSize = 36.sp, fontWeight =FontWeight.Bold,
-        color = Color.Blue
-        )
+//    Text(text = "hello Arjun", fontSize = 36.sp, fontWeight =FontWeight.Bold,
+//        color = Color.Blue
+//        )
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = "image"
+    )
 }
 
