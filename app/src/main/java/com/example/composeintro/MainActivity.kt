3a0cc26a38422ba7ccfe58d65f9d8f7f75ff495a
arjun98k <arjunkandekar14@gmail.com>
2025-03-31 20:49:34 +0530

    row and column

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -1,48 +1,77 @@
 package com.example.composeintro
 
+import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserInputScreen()
+//            UserInputScreen()
+            preview()
         }
     }
 }
 
+//@Composable
+//fun UserInputScreen() {
+//    var text by remember { mutableStateOf("") } // Remember state
+//
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(16.dp)
+//    ) {
+//        TextField(
+//            value = text,
+//            onValueChange = { text = it },
+//            label = { Text("Enter your name") },
+//            modifier = Modifier.fillMaxWidth()
+//        )
+//
+//        Spacer(modifier = Modifier.height(16.dp))
+//
+//        Text(text = "You entered: $text", style = MaterialTheme.typography.bodyLarge)
+//    }
+//}
+
 @Composable
-fun UserInputScreen() {
-    var text by remember { mutableStateOf("") } // Remember state
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("Enter your name") },
-            modifier = Modifier.fillMaxWidth()
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
+private  fun preview(){
+    ListViewItem()
+}
+@Composable
+fun ListViewItem(){
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null
         )
+        Modifier.size(50.dp)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Column {
+         Text(
 
-        Text(text = "You entered: $text", style = MaterialTheme.typography.bodyLarge)
+             text = "Arjun", fontWeight = FontWeight.Bold
+
+         )
+            Text(text = "Android Developer",
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 12.sp
+            )
+        }
     }
 }
 
-@Preview(showBackground = true, widthDp = 300, heightDp = 500, showSystemUi = true)
-@Composable
-fun PreviewFunction() {
-    UserInputScreen()
-}
